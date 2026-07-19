b3ecde7d52251d52ebe5ff81051efc3095d38f3a
Richard Feldman <oss@rtfeldman.com>
2026-07-19 08:23:16 -0400

    Add regression test for loop-carried list reads

--- src/compile/mod.zig
+++ src/compile/mod.zig
@@ -119,6 +119,7 @@ test "compile tests" {
     std.testing.refAllDecls(@import("test/issue_10021_test.zig"));
     std.testing.refAllDecls(@import("test/issue_10057_test.zig"));
     std.testing.refAllDecls(@import("test/issue_10132_test.zig"));
+    std.testing.refAllDecls(@import("test/issue_10218_test.zig"));
     std.testing.refAllDecls(@import("test/tce_capture_test.zig"));
     std.testing.refAllDecls(@import("test/list_map_target_independent_lir_test.zig"));
     std.testing.refAllDecls(@import("test/platform_box_update_lir_test.zig"));
